ded65d36d26dac2b80f9a6203144e036093cef7c
AggreyNhiwatiwa <aggrey.c.nhiwatiwa@gmail.com>
2024-12-07 16:30:00 -0500

    Moved Car related logic to Car component

--- components/Car/index.js
+++ components/Car/index.js
@@ -1,3 +1,39 @@
-import CarsScreen from './CarsScreen';
+import styles from "./styles";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import { View, Text, Image } from "react-native";
 
-export default CarsScreen;
+export default function Car({
+    brand,
+    model,
+    year,
+    pp,
+    drivetrain,
+    image,
+    credit,
+    isLimitedStock,
+}) {
+    return (
+        <View style={styles.card}>
+            <Image source={{ uri: image }} style={styles.image} />
+            <View style={styles.details}>
+                <Text style={styles.title}>{`${brand} ${model} '${year}`}</Text>
+                <View style={styles.chipContainer}>
+                    <Text style={styles.chip}>{`PP ${pp}`}</Text>
+                    <Text style={styles.drivetrain}>{drivetrain}</Text>
+                </View>
+                {isLimitedStock && (
+                    <View style={styles.limitedStockContainer}>
+                        <Icon name="warning" size={20} color="red" />
+                        <Text style={styles.limitedStockText}>
+                            Limited Stock
+                        </Text>
+                    </View>
+                )}
+                <View style={styles.creditContainer}>
+                    <Text style={styles.creditLabel}>Cr. </Text>
+                    <Text style={styles.credit}>{credit}</Text>
+                </View>
+            </View>
+        </View>
+    );
+}
--- screens/CarsScreen.js
+++ screens/CarsScreen.js
@@ -1,42 +1,32 @@
-import React, { useContext } from 'react';
-import { View, Text, FlatList, Image } from 'react-native';
-import { CarContext } from '../context/CarContext'; 
-import styles from '../components/Car/styles';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import React, { useContext } from "react";
+import { FlatList } from "react-native";
+import { CarContext } from "../context/CarContext";
+import Car from "../components/Car";
 
 const CarsScreen = () => {
-  const { cars } = useContext(CarContext);
+    const { cars } = useContext(CarContext);
 
-  const renderItem = ({ item }) => (
-    <View style={styles.card}>
-      <Image source={{ uri: item.image }} style={styles.image} />
-      <View style={styles.details}>
-        <Text style={styles.title}>{`${item.brand} ${item.model} '${item.year}`}</Text>
-        <View style={styles.chipContainer}>
-          <Text style={styles.chip}>{`PP ${item.pp}`}</Text>
-          <Text style={styles.drivetrain}>{item.drivetrain}</Text>
-        </View>
-        {item.isLimitedStock && (
-          <View style={styles.limitedStockContainer}>
-            <Icon name="warning" size={20} color="red" />
-            <Text style={styles.limitedStockText}>Limited Stock</Text>
-          </View>
-        )}
-        <View style={styles.creditContainer}>
-          <Text style={styles.creditLabel}>Cr. </Text>
-          <Text style={styles.credit}>{item.credit}</Text>
-        </View>
-      </View>
-    </View>
-  );
+    const renderItem = ({ item }) => (
+        <Car
+            id={item.id}
+            brand={item.brand}
+            model={item.model}
+            year={item.year}
+            pp={item.pp}
+            drivetrain={item.drivetrain}
+            image={item.image}
+            credit={item.credit}
+            isLimitedStock={item.isLimitedStock}
+        />
+    );
 
-  return (
-    <FlatList
-      data={cars}
-      renderItem={renderItem}
-      keyExtractor={(item) => item.id}
-    />
-  );
+    return (
+        <FlatList
+            data={cars}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+        />
+    );
 };
 
 export default CarsScreen;
