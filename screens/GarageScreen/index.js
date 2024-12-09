035df494717576bc53ce1accd964e32b6035bdad
AggreyNhiwatiwa <aggrey.c.nhiwatiwa@gmail.com>
2024-12-09 01:24:10 -0500

    Added information modal to garage screen

--- screens/GarageScreen/index.js
+++ screens/GarageScreen/index.js
@@ -1,14 +1,57 @@
 // React imports
 import React from "react";
-import { useContext } from "react";
-import { FlatList, View, Text } from "react-native";
+import { useContext, useEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+    FlatList,
+    Modal,
+    View,
+    Text,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+} from "react-native";
+
+// Third party imports
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 // Project imports
+import styles from "./styles";
 import { CarContext } from "../../context/CarContext";
 import Car from "../../components/Car";
 
 export default function GarageScreen() {
+    /* State */
     const { garageCars } = useContext(CarContext);
+    const navigation = useNavigation();
+
+    const [showModal, setShowModal] = useState(false);
+
+    /* Side effects */
+    useEffect(() => {
+        navigation.setOptions({
+            headerLeft: () => (
+                <TouchableOpacity
+                    onPress={handleShowModal}
+                    style={{ marginLeft: 15 }}
+                >
+                    <MaterialCommunityIcons
+                        name="information-outline"
+                        size={30}
+                        color="#000000"
+                    />
+                </TouchableOpacity>
+            ),
+        });
+    }, [navigation]);
+
+    /* Handlers */
+    const handleShowModal = () => {
+        setShowModal(true);
+    };
+
+    const handleHideModal = () => {
+        setShowModal(false);
+    };
 
     const renderItem = ({ item }) => (
         <Car
@@ -25,10 +68,30 @@ export default function GarageScreen() {
     );
 
     return (
-        <FlatList
-            data={garageCars}
-            renderItem={renderItem}
-            keyExtractor={(item) => item.id}
-        />
+        <>
+            <FlatList
+                data={garageCars}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.id}
+            />
+
+            <Modal visible={showModal} transparent={true} animationType="fade"> 
+                <TouchableWithoutFeedback onPress={handleHideModal}>
+                    <View style={styles.overlay}>
+                        <TouchableWithoutFeedback>
+                            <View style={styles.dialog}>
+                                <Text style={styles.heading}>
+                                    Managing your Garage
+                                </Text>
+                                <Text style={styles.dialogText}>
+                                    To remove a car from your garage, simply
+                                    tap the car you want to remove.
+                                </Text>
+                            </View>
+                        </TouchableWithoutFeedback>
+                    </View>
+                </TouchableWithoutFeedback>
+            </Modal>
+        </>
     );
 }
--- screens/GarageScreen/styles.js
+++ screens/GarageScreen/styles.js
@@ -1,5 +1,29 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    overlay: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0,0,0,0.5)',
+    },
+    dialog: {
+        width: 300,
+        height: 200,
+        textAlign: "center",
+        alignItems: "center",
+        padding: 20,
+        backgroundColor: 'white',
+        borderRadius: 10,
+    },
+    heading: {
+        fontSize: 20,
+        fontWeight: "600",
+        marginBottom: 20,
+    },
+    dialogText: {
+        fontSize: 16,
+    },
+});
 
 export default styles;
