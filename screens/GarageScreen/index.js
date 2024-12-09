a81f7c44a746a0d3f058bb9444c2d60e56cdde49
S. Berk Ozdemir <sberk99@hotmail.com>
2024-12-09 02:24:11 -0500

    filter added for garage screen

--- screens/GarageScreen/index.js
+++ screens/GarageScreen/index.js
@@ -1,6 +1,5 @@
 // React imports
-import React from "react";
-import { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import {
     FlatList,
@@ -13,11 +12,13 @@ import {
 
 // Third party imports
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import Icon from 'react-native-vector-icons/Ionicons';
 
 // Project imports
 import styles from "./styles";
 import { CarContext } from "../../context/CarContext";
 import Car from "../../components/Car";
+import CarFilterDialog from "../../components/dialog/CarFilterDialog";
 
 export default function GarageScreen() {
     /* State */
@@ -25,6 +26,14 @@ export default function GarageScreen() {
     const navigation = useNavigation();
 
     const [showModal, setShowModal] = useState(false);
+    const [showFilterDialog, setShowFilterDialog] = useState(false);
+    const [filters, setFilters] = useState({
+        limitedStock: false,
+        minPp: 0,
+        maxPp: 1500,
+        minCredit: 0,
+        maxCredit: 500000,
+    });
 
     /* Side effects */
     useEffect(() => {
@@ -41,6 +50,14 @@ export default function GarageScreen() {
                     />
                 </TouchableOpacity>
             ),
+            headerRight: () => (
+                <Icon
+                    name="filter"
+                    size={25}
+                    style={{ marginRight: 15 }}
+                    onPress={() => setShowFilterDialog(true)}
+                />
+            ),
         });
     }, [navigation]);
 
@@ -52,6 +69,28 @@ export default function GarageScreen() {
     const handleHideModal = () => {
         setShowModal(false);
     };
+
+    const handleShowFilterDialog = () => {
+        setShowFilterDialog(true);
+    };
+
+    const handleHideFilterDialog = () => {
+        setShowFilterDialog(false);
+    };
+
+    const handleApplyFilters = (newFilters) => {
+        setFilters(newFilters);
+    };
+
+    const filteredCars = garageCars.filter(car => {
+        return (
+            car.pp >= filters.minPp &&
+            car.pp <= filters.maxPp &&
+            car.credit >= filters.minCredit &&
+            car.credit <= filters.maxCredit &&
+            (!filters.limitedStock || car.isLimitedStock)
+        );
+    });
 
     const renderItem = ({ item }) => (
         <Car
@@ -70,7 +109,7 @@ export default function GarageScreen() {
     return (
         <>
             <FlatList
-                data={garageCars}
+                data={filteredCars}
                 renderItem={renderItem}
                 keyExtractor={(item) => item.id}
             />
@@ -92,6 +131,13 @@ export default function GarageScreen() {
                     </View>
                 </TouchableWithoutFeedback>
             </Modal>
+
+            <CarFilterDialog
+                visible={showFilterDialog}
+                onClose={handleHideFilterDialog}
+                onApply={handleApplyFilters}
+                limitedStock={filters.limitedStock}
+            />
         </>
     );
 }
